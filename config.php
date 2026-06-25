7f0ffcaea30e37b4c3198a50903725b29aeaaf4d
Raven Baluyot <avenbaluyot05@gmail.com>
2026-06-25 23:39:04 +0800

    second commit

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $conn = mysqli_connect("localhost", "root", "", "users_db");
 
 if(!$conn) {
-  die("Connection wow failed: " . mysqli_connect_error());
+  die("Connection failed: " . mysqli_connect_error());
 }
